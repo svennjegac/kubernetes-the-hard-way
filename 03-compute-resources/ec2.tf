88ea7ff399bdbc2632f9336438a8147e89977eca
Sven Njegac <sven.njegac@gmail.com>
2022-02-23 02:06:19 +0100

    feat: add workers

--- 03-compute-resources/ec2.tf
+++ 03-compute-resources/ec2.tf
@@ -73,7 +73,26 @@ resource "aws_instance" "k8s_control_plane" {
   security_groups = [aws_security_group.k8s_security_group.id]
 
   tags = {
-    Name = "k8s_control_plane_${count.index}"
+    Name = "k8s_controller_${count.index}"
+  }
+
+  count = 3
+}
+
+resource "aws_instance" "k8s_worker_plane" {
+  ami           = local.ubuntu_ami
+  instance_type = local.instance_type
+
+  associate_public_ip_address = true
+  availability_zone           = local.subnet_az
+  key_name                    = aws_key_pair.k8s_key_pair.key_name
+
+  subnet_id       = aws_subnet.k8s_main.id
+  private_ip      = "10.240.0.2${count.index}"
+  security_groups = [aws_security_group.k8s_security_group.id]
+
+  tags = {
+    Name = "k8s_worker_${count.index}"
   }
 
   count = 3
