b45694706af95fa2f477540718ddcc17f4f7a420
Sven Njegac <sven.njegac@gmail.com>
2022-02-26 14:43:58 +0100

    boot etcd

--- 07-bootstrap-etcd/07_bootstrap.sh
+++ 07-bootstrap-etcd/07_bootstrap.sh
@@ -1,11 +1,25 @@
 #!/bin/zsh
 
+set -e
 
+while read line; do
+splits=("${(@s/ /)line}")
+instance=$splits[1]
+private_ip=$splits[2]
+public_ip=$splits[3]
 
-ssh
-
+ssh -tt -i ../99_shared/03_k8s_ssh_key ubuntu@$public_ip <<EOF
 wget -q --show-progress --https-only --timestamping "https://github.com/etcd-io/etcd/releases/download/v3.4.15/etcd-v3.4.15-linux-amd64.tar.gz"
+tar -xf etcd-v3.4.15-linux-amd64.tar.gz
+sudo mv etcd-v3.4.15-linux-amd64/etcd* /usr/local/bin/
+sudo mkdir -p /etc/etcd /var/lib/etcd
+sudo chmod 700 /var/lib/etcd
+sudo cp ca.pem kubernetes-key.pem kubernetes.pem /etc/etcd/
+sudo sh etcd_service_maker.sh
+sudo systemctl daemon-reload
+sudo systemctl enable etcd
+sudo systemctl start etcd &
+exit
+EOF
 
-export INTERNAL_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-
-export ETCD_NAME=`cat hostname.txt`
+done <./../99_shared/03_controllers.txt
--- 07-bootstrap-etcd/07_prepare_hosts.sh
+++ 07-bootstrap-etcd/07_prepare_hosts.sh
@@ -9,7 +9,7 @@ private_ip=$splits[2]
 public_ip=$splits[3]
 
 echo -n $instance > hostname.txt
-scp -i ../99_shared/03_k8s_ssh_key hostname.txt ubuntu@$public_ip:~/
+scp -i ../99_shared/03_k8s_ssh_key hostname.txt etcd_service_maker.sh ubuntu@$public_ip:~/
 rm hostname.txt
 
 done <./../99_shared/03_controllers.txt
